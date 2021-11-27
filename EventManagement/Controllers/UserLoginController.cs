195242b70c029e2fe92e3b7d55488aab3289894a
TharunStart <tharunmrock@gmail.com>
2021-11-27 10:48:00 +0530

    my
    New Implimentation

---
+++ EventManagement/Controllers/UserLoginController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EventManagement.Model;
+
+namespace EventManagement.Controllers
+{
+    public class UserLoginController : Controller
+    {
+        // GET: UserLogin
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult answer(EventManagement.Model.user details)
+        { 
+            using(eventsEntities1 db = new eventsEntities1())
+            {
+                var userdetails = db.users.Where(x => x.name == details.name && x.password == details.password).FirstOrDefault();
+                if (userdetails == null)
+                {
+                    details.LoginErrorMessage = "Invalid User";
+                    return View("Login", details);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "users");
+                }
+            }
+            return View();
+        }
+    }
+}
